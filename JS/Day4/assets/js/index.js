c9c90114676fce9625619a705514ff5d9fbe5c57
JananiS1418 <jananisomasundaram07@gmail.com>
2026-08-21 10:12:33 +0530

    updated

---
+++ JS/Day4/assets/js/index.js
@@ -0,0 +1,102 @@
+// // switch (expression) {
+
+// //     case value1:
+// //         // code
+// //         break
+// //     case value2:
+// //         // code
+// //         break
+// //     default
+// //        // code
+
+// // }
+
+
+// let course = "456"
+
+// switch (course) {
+
+//  case "HTML":
+//     console.log("This is Not My Course");
+//     break
+//  case 2:
+//     console.log("TuesDay");
+//     break
+//  case 3:
+//     console.log("Wednesday");
+//     break
+//  case "React":
+//     console.log("thursday");
+//     break
+//  case 5:
+//     console.log("friday");
+//     break
+// default:
+//    console.log("Invalid date");
+       
+
+
+// }
+// let mark = 85
+
+// if(mark >= 90){
+
+// console.log("A+");
+
+
+// }else if(mark <= 100){
+//     console.log("A+");
+    
+// }
+
+// else {
+
+//     console.log('Only valid 100');
+    
+
+// }
+
+
+// let username = "admin";
+
+// let password = "123674";
+
+
+// if (username==="admin" && password === "1234") {
+
+//     console.log('Login Successfull');
+    
+    
+// }else {
+//     console.log("Invalid");
+    
+// }
+
+
+
+
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+
+// for
+// for (init;condition;incrment/decrement){
+
+
+
+// }
+// while
+// do while
+
+for (let a = 10; a > 0 ; a--){
+
+    console.log(a);
+    
+}
+
