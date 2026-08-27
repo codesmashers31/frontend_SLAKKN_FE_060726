c409b84d3efe0ae01e832139e0c3d71d4a753d39
JananiS1418 <jananisomasundaram07@gmail.com>
2026-08-27 09:55:20 +0530

    updated

---
+++ JS/Day8/assets/js/index.js
@@ -0,0 +1,72 @@
+// function vadachennai (){
+
+
+
+
+// if(true){
+
+// var hero = "Dhanush"
+// let heroone = "Rajan"
+// const herotwo = "Arasan"
+
+
+
+// console.log('hero',hero);
+// console.log('heroone',heroone);
+// console.log('herotwo',herotwo);
+
+// }
+
+
+// console.log('hero',hero);
+
+
+
+// }
+
+// // console.log('hero',hero);
+// // console.log('heroone',heroone);
+// // console.log('herotwo',herotwo);
+
+// vadachennai()
+
+
+
+
+
+// var a;
+
+// add()
+
+// let add =  ()=>{
+//     console.log('running');
+    
+// }
+
+
+const add = (uesr)=>{
+    
+    console.log(a);
+    
+   let sum  = 0
+   for(let a = 0;a<=uesr;a++){
+      
+    sum += a
+      
+   }
+
+   
+
+   let a = 10
+
+   return sum
+
+}
+    
+
+
+console.log(add());
+
+
+
+
