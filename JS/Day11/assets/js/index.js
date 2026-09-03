22d5b96a6bcb74070547e2c72df654ce54cf677b
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-03 10:07:42 +0530

    Updated day 12

---
+++ JS/Day11/assets/js/index.js
@@ -0,0 +1,104 @@
+// // const arr = [1,2,3,4,5]
+
+// // console.log('original array', arr);
+
+// // arr.push(30)
+
+// // const dataAdd = arr.push(40)
+
+// // console.log('dataAdd',dataAdd);
+
+// // console.log('after the edit', arr);
+
+// // console.log("original" , arr);
+
+
+// // const copy = arr.slice(0,2)
+
+// // console.log('copy' , copy);
+
+
+
+// // console.log('original' , arr);
+
+
+
+
+
+// // console.log(arr);
+
+
+// // console.log(arr.includes(3));
+
+
+// // console.log(arr);
+
+
+// const arr = [1,2,3,4,5]
+
+// arr.unshift(34)
+// const i= arr.unshift(22)
+// console.log(i)
+
+// arr.slice(0,2)
+// console.log(arr);
+
+
+
+// const copy = 1
+
+// console.log(copy);
+
+
+
+// // HOAM
+
+//  arr.forEach((e,i)=>{
+
+//    console.log(e%2==0?"even":"odd");
+   
+
+// })
+// // console.log(run);
+
+
+// console.log(arr);
+
+// const newarr = arr.map((i,e)=>{
+
+
+
+//    return i
+   
+
+// })
+
+// console.log(newarr);
+
+
+
+
+const arr = [12,3,4,5,6]
+
+let fdgt = ""
+
+const newdsr = arr.forEach((e,i)=>{
+
+   const  tdgt = e + i 
+
+   fdgt += tdgt
+
+  
+   
+})
+
+console.log(fdgt);
+
+
+const newarr = arr.map((e,i)=>{
+
+   return e*i
+   
+})
+
+console.log(newarr);
---
+++ JS/Day12/assets/js/index.js
@@ -0,0 +1,26 @@
+// console.log(document);
+// const title = document.getElementById("title")
+
+// console.log(title)
+
+// document.getElementsByClassName("titles")
+// console.log(document.getElementsByTagName("p"));
+
+// document.querySelector("#title")
+// document.querySelectorAll()
+
+
+
+
+const titleData = document.getElementById("title")
+const btnData = document.getElementById("btn")
+const link = document.getElementsByTagName("img")
+
+btnData.addEventListener("click",()=>{
+
+  
+console.log(titleData);
+
+   
+
+})
