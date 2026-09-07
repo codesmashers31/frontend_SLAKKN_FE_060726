69eb9dd9aeff70d82b765f77acb1f82e62839217
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-07 09:59:40 +0530

    updated

---
+++ JS/Day13/assets/js/index.js
@@ -0,0 +1,85 @@
+
+
+    // const title = document.getElementById("title")
+    // const btn = document.getElementById("btn")
+
+    // const imgs = document.getElementById("imgs")
+
+    // const link = document.getElementById("link")
+    
+    // btn.addEventListener("click",()=>{
+
+    //      title.textContent = "This is the Text"
+
+    //      imgs.src = "assets/images/logo/logo.png";
+         
+
+    //      link.href = "https://creatorapp.zohopublic.in/info_softlogicsys/sla/report-perma/Batches_for_Trainers/OxKyw5WBD0zb2YQChz3TKHTy0sPPZSnJeOpgu3dadsM1unyUwJ4mNTkWke00bDeHFXU0vbgZpv6xDT91UZCsuFTKOHwu8bfG7TjE?&Location=KK%20Nagar&Trainer_Name=Balamugunthan%20S"
+
+    //     //  window.location.href = "https://creatorapp.zohopublic.in/info_softlogicsys/sla/report-perma/Batches_for_Trainers/OxKyw5WBD0zb2YQChz3TKHTy0sPPZSnJeOpgu3dadsM1unyUwJ4mNTkWke00bDeHFXU0vbgZpv6xDT91UZCsuFTKOHwu8bfG7TjE?&Location=KK%20Nagar&Trainer_Name=Balamugunthan%20S"
+
+    // })
+
+   
+const showbtn = document.getElementById("showbtn")
+const box = document.getElementById("box1")
+
+
+showbtn.addEventListener("click",()=>{
+    //console.log(box);
+    
+    //console.log(box.style.display);
+
+     box.classList.toggle("show")
+    
+
+    // if(box.style.display === "none") {
+     
+    //     box.style.display = "block"
+    //     box.classList.toggle = "show"
+
+
+    // }else{
+
+    //      box.style.display = "none"
+    //      box.classList.toggle = "show"
+    // }
+
+})
+
+
+
+
+
+
+
+// let ison = false
+
+// showbtn.addEventListener("click",()=>{
+
+//  ison = !ison
+
+
+//  //console.log(box);
+ 
+//    if(ison){
+
+  
+//    showbtn.textContent = "Blue"
+//    box.style.backgroundColor = "blue"
+
+//    }else {
+
+   
+//    showbtn.textContent = "Red"
+//    box.style.backgroundColor = "red"
+
+//    }
+
+
+
+   
+
+
+
+// })
