c999b19d132d8dc240186099099d33768c4005d4
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-24 09:58:45 +0530

    done udpate true

--- React/Day8/usestatehooks/src/App.jsx
+++ React/Day8/usestatehooks/src/App.jsx
@@ -54,44 +54,83 @@ import { useState } from "react"
 
 
 
+// const App = () => {
+
+//   console.log('Component Rerender');
+  
+
+//   const [countNumber,setCountNumber] = useState(0)
+  
+  
+//   const handleClick = ()=>{
+
+//     setCountNumber(countNumber+1)
+
+//   }
+
+//   const handleDec = ()=>{
+
+//     setCountNumber(countNumber-1)
+
+//   }
+
+//   const handleReset = ()=>{
+
+//     setCountNumber(0)
+
+//   }
+
+
+//   return (
+//       <>
+      
+//       <div>
+//         <h1>{countNumber}</h1>
+//         <button onClick={handleClick}>Count (++)</button>
+//         <button onClick={handleDec}>Count (--)</button>
+//         <button onClick={handleReset}>Reset</button>
+//       </div>
+      
+//       </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
 const App = () => {
 
-  console.log('Component Rerender');
-  
+  const [title,setTitle] = useState("This is react")
+ 
+  const [isActive,setIsActive] = useState(true)
 
-  const [countNumber,setCountNumber] = useState(0)
-  
-  
-  const handleClick = ()=>{
+  const changetext = ()=>{
 
-    setCountNumber(countNumber+1)
+      setTitle("This is Node")
 
   }
 
-  const handleDec = ()=>{
+  const SHowText = ()=>{
 
-    setCountNumber(countNumber-1)
-
-  }
-
-  const handleReset = ()=>{
-
-    setCountNumber(0)
+    setIsActive(!isActive)
 
   }
-
 
   return (
-      <>
+    <>
+
+    <h3>{title}</h3>
+    <button onClick={changetext}>Click To change</button>
+    
       
-      <div>
-        <h1>{countNumber}</h1>
-        <button onClick={handleClick}>Count (++)</button>
-        <button onClick={handleDec}>Count (--)</button>
-        <button onClick={handleReset}>Reset</button>
-      </div>
-      
-      </>
+    {isActive&&<p>This is React</p>}  
+
+    <button onClick={SHowText}>{isActive?"Show":"Hide"}</button>
+    </>
   )
 }
 
