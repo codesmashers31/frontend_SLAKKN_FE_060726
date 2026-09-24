3a7e46d9d6e5e9a450d25f318562fa6a1b483213
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-24 09:19:07 +0530

    React update phase

---
+++ React/Day8/usestatehooks/src/App.jsx
@@ -0,0 +1,98 @@
+// import { useState } from "react"
+
+import { useState } from "react"
+
+// const App = ()=>{
+
+//   console.log('Compopnent Running');
+  
+
+// // datas 
+// // countNumber - setCountNumber
+//   let [constNumber,setCountNumber] = useState(10)
+//   // datas - state value
+//   // setDatas - setstate function
+//   // useState - hooks
+//   // 10 - datatypes - inital value
+
+//   let data = 10
+
+
+//   const handleClick = ()=>{
+    
+    
+//     setCountNumber(constNumber+1)
+
+       
+
+//   }
+
+
+  
+
+//   return (<>
+  
+  
+//    <div>
+//     APP
+    
+//     <p>{constNumber}</p>
+
+//     <button onClick={handleClick}>Click Now</button>
+//    </div>
+
+
+//   </>)
+
+
+// }
+
+// export default App
+
+
+
+
+
+
+const App = () => {
+
+  console.log('Component Rerender');
+  
+
+  const [countNumber,setCountNumber] = useState(0)
+  
+  
+  const handleClick = ()=>{
+
+    setCountNumber(countNumber+1)
+
+  }
+
+  const handleDec = ()=>{
+
+    setCountNumber(countNumber-1)
+
+  }
+
+  const handleReset = ()=>{
+
+    setCountNumber(0)
+
+  }
+
+
+  return (
+      <>
+      
+      <div>
+        <h1>{countNumber}</h1>
+        <button onClick={handleClick}>Count (++)</button>
+        <button onClick={handleDec}>Count (--)</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+      
+      </>
+  )
+}
+
+export default App
